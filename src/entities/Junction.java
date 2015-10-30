a7acbed157e80a3226ae3a456dd1922c765a0112
Fabian Stiehle <fabianstiehle@Fabians-MBP.fritz.box>
2015-10-30 18:44:10 +0100

    implemented additional set methods

--- src/entities/Junction.java
+++ src/entities/Junction.java
@@ -4,16 +4,39 @@ package entities;
  * a Junction knows its neighbours
  */
 public class Junction {
+    coordinates = {x,y};
+
     Junction up;
     Junction right;
 
     Junction down;
     Junction left;
 
-    public class Junction(Junction up, Junction right, Junction down, Junction left) {
+    public class Junction() {
         this.up = up;
         this.right = right;
         this.down = down;
+        this.left = left;
+    }
+
+    public class setPosition(x,y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public class addUp(Junction r) {
+        this.up = up;
+    }
+
+    public class addRight(Junction r) {
+        this.right = right;
+    }
+
+    public class addDown(Junction r) {
+        this.down = down;
+    }
+
+    public class addLeft(Junction r) {
         this.left = left;
     }
 }
