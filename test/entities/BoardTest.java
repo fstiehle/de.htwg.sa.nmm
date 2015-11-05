c50165cf38c95d58b0c621acae952bcbababa848
Fabian Stiehle <fabianstiehle@Fabians-MBP.fritz.box>
2015-11-05 20:41:11 +0100

    deleted false implementation of test cases

--- test/entities/BoardTest.java
+++ test/entities/BoardTest.java
@@ -1,22 +1,5 @@
 package entities;
 
-import static junit.Assert.*;
-
-import junit.Before;
-import junit.Test;
-/**
- * Created by fabianstiehle on 30.10.15.
- */
 public class BoardTest {
-    Board board;
 
-    @Before
-    public void setUp() throws Exception {
-        board = new Board();
-    }
-
-    @Test
-    public Board testGetBoard() {
-
-    }
 }
