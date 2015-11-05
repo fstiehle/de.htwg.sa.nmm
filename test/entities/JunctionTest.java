3ce22257a0cb975430e95e9672c80ce481497773
Fabian Stiehle <fabianstiehle@Fabians-MBP.fritz.box>
2015-11-05 21:10:21 +0100

    fixed test environment

--- test/entities/JunctionTest.java
+++ test/entities/JunctionTest.java
@@ -1,8 +1,70 @@
 package entities;
 
-/**
- *
- */
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JunctionTest {
 
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPosition() throws Exception {
+
+    }
+
+    @Test
+    public void testSetNeighbours() throws Exception {
+
+    }
+
+    @Test
+    public void testSetUp1() throws Exception {
+
+    }
+
+    @Test
+    public void testSetRight() throws Exception {
+
+    }
+
+    @Test
+    public void testSetDown() throws Exception {
+
+    }
+
+    @Test
+    public void testSetLeft() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPositionX() throws Exception {
+
+    }
+
+    @Test
+    public void testSetPositionY() throws Exception {
+
+    }
+
+    @Test
+    public void testEquals() throws Exception {
+
+    }
+
+    @Test
+    public void testHashCode() throws Exception {
+
+    }
 }
