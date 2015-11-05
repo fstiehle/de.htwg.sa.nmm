7d1e42c80c6778ccb897950a4cbd063e3f0b2596
Fabian Stiehle <fabianstiehle@Fabians-MBP.fritz.box>
2015-11-05 21:03:02 +0100

    WIP: Board constructor to supply junction connections

--- src/entities/Board.java
+++ src/entities/Board.java
@@ -1,17 +1,35 @@
 package entities;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * Builds and Returns the main board
  * The Board is comprised of Junctions
- * 49 junctions
+ * 24 junctions
  */
-public class Board
+public class Board implements BoardSetup {
 
-    Junction 1x1 = new Junction(null,null,null,null);
+    Map<String, Junction> boardMap = new HashMap<>();
 
-    public Board Board() {
+    public Map<String, Junction> Board() {
 
+        //Connections
+        // directions clockwise: up, right, down, left
+        //a1---------a4--------a7
+        a1.setNeighbours(null, a4, d1, null);
+        a4.setNeighbours(null, a7, b4, a1);
+        a7.setNeighbours(null, null, d7, a4);
+
+        //----b2-----b4-----b6----
+
+        return boardMap;
     }
 
-
+    @Override
+    public String toString() {
+        return "Board{" +
+                "boardMap=" + boardMap +
+                '}';
+    }
 }
