3583f941548d13e401855e67d02ef254d35f95cc
Fabian Stiehle <fabianstiehle@Fabians-MacBook-Pro.local>
2015-11-13 12:41:25 +0100

    main class

--- src/Game.java
+++ src/Game.java
@@ -1,8 +1,18 @@
 /**
  * Created by mafunke on 23.10.2015.
  */
+
+import java.util.Scanner;
+import controller.GameController;
+import entities.Board;
+import tui.TextUI;
+
 public class Game {
+
+    static Scanner scanner;
+
     public static void main(String[] args) {
+        TextUI textUI = new TextUI(new GameController(new Board()));
 
     }
 }
